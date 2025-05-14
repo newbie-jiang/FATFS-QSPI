05dd0943abc85dfb447a7778f5c08e71777b3694
何德江 <2995560796@qq.com>
2025-05-14 17:01:51 +0800

    last update

--- MDK-ARM/STM32H750XB/bsp_sd.d
+++
@@ -1,2 +0,0 @@
-stm32h750xb\bsp_sd.o: ..\BSP\bsp_sd.c
-stm32h750xb\bsp_sd.o: ..\BSP\bsp_sd.h
--- MDK-ARM/STM32H750XB/ffunicode.d
+++
@@ -1,4 +0,0 @@
-stm32h750xb\ffunicode.o: ..\ftafs\source\ffunicode.c
-stm32h750xb\ffunicode.o: ..\ftafs\source\ff.h
-stm32h750xb\ffunicode.o: ..\ftafs\source\ffconf.h
-stm32h750xb\ffunicode.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
